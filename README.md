d2bc87fc84cd423d17b54b84af0bc7994ca4f202
Raúl Juárez <juarezjaramillo@gmail.com>
2014-10-12 13:23:09 -0500

    Change API
    Change ‘open’ to ‘pick’ and added ‘type’ param

--- README.md
+++ README.md
@@ -4,25 +4,28 @@ Requires Cordova >= 2.8.0
 
 Install with Cordova CLI
 	
-	$ cordova plugin add http://github.com/don/cordova-filechooser.git
+	$ cordova plugin add http://github.com/juarezjaramillo/cordova-filechooser.git
 
 Install with Plugman 
 
 	$ plugman --platform android --project /path/to/project \ 
-		--plugin http://github.com/don/cordova-filechooser.git
+		--plugin http://github.com/juarezjaramillo/cordova-filechooser.git
 
 API
 
-	fileChooser.open(successCallback, failureCallback);
+	fileChooser.pick(type, successCallback, failureCallback);
+
+where type can be "audio", "video", "image" and "*".
 
 The success callback get the uri of the selected file
 
-	fileChooser.open(function(uri) {
+	fileChooser.pick("image",function(uri) {
 		alert(uri);
 	});
+
 	
 Screenshot
 
 ![Screenshot](filechooser.png "Screenshot")
 
-TODO rename `open` to pick, select, or choose.
+
--- src/android/FileChooser.java
+++ src/android/FileChooser.java
@@ -1,20 +1,25 @@
 package com.megster.cordova;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
+import android.provider.OpenableColumns;
 import android.util.Log;
 
-import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 public class FileChooser extends CordovaPlugin {
 
     private static final String TAG = "FileChooser";
     private static final String ACTION_OPEN = "open";
+    private static final String ACTION_PICK = "pick";
     private static final int PICK_FILE_REQUEST = 1;
     CallbackContext callback;
 
@@ -22,19 +27,25 @@ public class FileChooser extends CordovaPlugin {
     public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
 
         if (action.equals(ACTION_OPEN)) {
-            chooseFile(callbackContext);
+            chooseFile(callbackContext,null);
+            return true;
+        }else if(action.equals(ACTION_PICK)){
+            chooseFile(callbackContext,args.getString(0));
             return true;
         }
 
         return false;
     }
 
-    public void chooseFile(CallbackContext callbackContext) {
+    public void chooseFile(CallbackContext callbackContext,String type) {
 
         // type and title should be configurable
-
         Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-        intent.setType("*/*");
+        if("audio".equals(type)||"video".equals(type)||"image".equals(type)){
+            intent.setType(type.concat("/*"));
+        }else {
+            intent.setType("*/*");
+        }
         intent.addCategory(Intent.CATEGORY_OPENABLE);
 
         Intent chooser = Intent.createChooser(intent, "Select File");
@@ -44,7 +55,7 @@ public class FileChooser extends CordovaPlugin {
         pluginResult.setKeepCallback(true);
         callback = callbackContext;
         callbackContext.sendPluginResult(pluginResult);
-        
+
     }
 
     @Override
@@ -57,33 +68,32 @@ public class FileChooser extends CordovaPlugin {
                 Uri uri = data.getData();
                 int nameIndex = -1;
                 int sizeIndex = -1;
-                
+
                 if (uri != null) {
-                	Context context=this.cordova.getActivity().getApplicationContext();
+                    Context context=this.cordova.getActivity().getApplicationContext();
                     Log.w(TAG, uri.toString());
                     JSONObject jo = new JSONObject();
                     String mimeType = context.getContentResolver().getType(uri);
                     Cursor returnCursor = context.getContentResolver().query(uri, null, null, null, null);
                     if(returnCursor!=null)
                     {
-                    	nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
+                        nameIndex = returnCursor.getColumnIndex(OpenableColumns.DISPLAY_NAME);
                         sizeIndex = returnCursor.getColumnIndex(OpenableColumns.SIZE);
                         returnCursor.moveToFirst();
                     }
-                    
-                    
+
+
                     try {
-						jo.put("url", uri.toString());
-						jo.put("type", mimeType);
-						if(sizeIndex>-1)
-						{
-							jo.put("size", Long.toString(returnCursor.getLong(sizeIndex)));
-							jo.put("filename", returnCursor.getString(nameIndex));
-						}
-					} catch (JSONException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+                        jo.put("url", uri.toString());
+                        jo.put("type", mimeType);
+                        if(sizeIndex>-1)
+                        {
+                            jo.put("size", Long.toString(returnCursor.getLong(sizeIndex)));
+                            jo.put("filename", returnCursor.getString(nameIndex));
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
                     callback.success(jo.toString());
 
                 } else {
